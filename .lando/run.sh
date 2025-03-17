94a17b71adfc02a21f26b2db06f0b2fa4b94801d
jackysmith040 <jackysmith040@gmail.com>
2025-03-17 01:37:14 +0000

    add configs

---
+++ .lando/run.sh
@@ -0,0 +1,7 @@
+if [ ! -d "./wp-admin" ]; then
+	wp core download
+fi
+
+if ! $(wp core is-installed); then
+	wp core install --url="http://$LANDO_APP_NAME.lndo.site" --title="WP Starter" --admin_user="admin" --admin_password="password" --admin_email="admin@$LANDO_APP_NAME.lndo.site" --skip-email
+fi
